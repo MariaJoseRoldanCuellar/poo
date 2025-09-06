fc6e4ec700fc776601f3286c4c369ed8218159d0
Mchcc-00 <Bleachowl98@gmail.com>
2025-09-05 20:18:48 -0500

    creacion de constructor

---
+++ constructor/carro.php
@@ -0,0 +1,30 @@
+<?php
+
+class carro{
+    public $color;
+    public $marca;
+    public $modelo;
+    public $velocidad;
+    public $caballaje;
+    public $puestos;
+
+
+    // public function __construct(){
+    //       $this->color='rojo';
+    //       $this->marca='Ferrari';
+    //       $this->modelo='Gallardo';
+    //       $this->velocidad=500;
+    //       $this->caballaje=300;
+    //       $this->puestos=2;
+    //     }
+
+    
+    public function __construct($color,$marca,$modelo,$velocidad,$caballaje,$puestos){
+          $this->color=$color;
+          $this->marca=$marca;
+          $this->modelo=$modelo;
+          $this->velocidad=$velocidad;
+          $this->caballaje=$caballaje;
+          $this->puestos=$puestos;
+    }
+}
---
+++ constructor/index.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'carro.php';
+
+//$carro1= new carro();
+
+$carro1=new carro ("amarillo","ferrari","gallardo",500,200,2);
+$carro2=new carro ("lila","lamborgini","alpha",1500,200,2);
+$carro3=new carro ("negro","audi","A5",1200,200,4);
+$carro4=new carro ("rosa","mercedes","clase a",500,200,2);
+
+
+var_dump($carro1);
+var_dump($carro2);
+var_dump($carro3);
+var_dump($carro4);
+
